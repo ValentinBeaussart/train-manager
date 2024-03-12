d6ea398b87ebb5a290f0b6818624e9976aef5ff2
ValentinBeaussart <valentin.beaussart@gmail.com>
2024-03-12 14:26:39 +0100

    fix order mocking

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
-Train.create!([plateform: 'A', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 1])
-Train.create!([plateform: 'B', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 2])
-Train.create!([plateform: 'C', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 3])
-Train.create!([plateform: 'D', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 4])
-Train.create!([plateform: 'E', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 5])
-Train.create!([plateform: 'F', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 6])
-Train.create!([plateform: 'G', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 7])
-
 Destination.create!([name: 'A', station_code: 'A', category: 'A'])
 Destination.create!([name: 'B', station_code: 'B', category: 'B'])
 Destination.create!([name: 'C', station_code: 'C', category: 'C'])
@@ -23,3 +14,11 @@ Destination.create!([name: 'D', station_code: 'D', category: 'D'])
 Destination.create!([name: 'E', station_code: 'E', category: 'E'])
 Destination.create!([name: 'F', station_code: 'F', category: 'F'])
 Destination.create!([name: 'G', station_code: 'G', category: 'G'])
+
+Train.create!([platform: 'A', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 1])
+Train.create!([platform: 'B', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 2])
+Train.create!([platform: 'C', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 3])
+Train.create!([platform: 'D', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 4])
+Train.create!([platform: 'E', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 5])
+Train.create!([platform: 'F', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 6])
+Train.create!([platform: 'G', arrival_time: '2024-03-11 23:25:52', departure_time: '2024-03-11 23:25:52', destination_id: 7])
