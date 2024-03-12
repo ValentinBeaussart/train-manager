3c81d5fe987942360665b5c8dbda43f90de6a642
ValentinBeaussart <valentin.beaussart@gmail.com>
2024-03-12 14:24:12 +0100

    migration for user table using devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_11_232552) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_234716) do
   create_table "destinations", force: :cascade do |t|
     t.string "name"
     t.string "station_code"
@@ -25,6 +25,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_232552) do
     t.datetime "departure_time"
     t.integer "destination_id", null: false
     t.index ["destination_id"], name: "index_trains_on_destination_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "trains", "destinations"
