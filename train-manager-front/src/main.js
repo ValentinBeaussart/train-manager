ea31c70e160c9d6832d6ef16cd91df0072423de2
ValentinBeaussart <valentin.beaussart@gmail.com>
2024-03-12 14:19:51 +0100

    import axios

--- train-manager-front/src/main.js
+++ train-manager-front/src/main.js
@@ -6,11 +6,13 @@ import router from './router'
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(Axios)
 
 /* eslint-disable no-new */
 new Vue({
